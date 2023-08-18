9305c54fd6aaf97e498f602b9cde0c44f593595a
VAIBHAV CHAUHAN <108172053+Vaibhav2003Chauhan@users.noreply.github.com>
2023-08-18 15:14:26 +0530

    download solve

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-const genmeme=document.getElementById("next");
-const genimage=document.getElementById("meme");
-const gent=document.getElementById("title");
-const gena=document.getElementById("author");
 
-const updatedetail=(url , title ,author)=>{
-    genimage.setAttribute("src",url);
-    gent.innerHTML="Title : "+ title;
-    gena.innerHTML="Author : "+author;
+const genmeme = document.getElementById("next")
+api = "https://meme-api.com/gimme"
+
+// this fetch was done to show up a initial meme as we go to another tab or just open up the app
+fetch(api).then((response) => response.json().then(data => {
+    console.log(data)
+    document.getElementById("meme").src = data.url
+    document.getElementById("title").innerHTML = "Title :" + data.title;
+    document.getElementById("author").innerHTML = "Author :" + data.author
+    document.getElementById("download1").src = data.url
+}));
+
+function generate() {
+
+// this function was there to propogate new memes from the existing memes 
+    fetch(api).then((response) => response.json().then(data => {
+        console.log(data)
+        document.getElementById("meme").src = data.url
+        document.getElementById("title").innerHTML = "Title :" + data.title;
+        document.getElementById("author").innerHTML = "Author :" + data.author
+        document.getElementById("download1").src = data.url
+    }));
 
 }
 
-const generate =() =>{
- fetch( "https://meme-api.com/gimme").then((response) => response.json()).then(data =>{
-        updatedetail(data.url,data.title,data.author);
-    });
-};
 
+function download() {
+    // window.location.href=document.getElementById("download1").src
+    let url = document.getElementById("download1").src
+    // window open was used out to load page in new tab above method was alos be used 
+    window.open(url, '_blank')
+}
 
+// this function is a part of navigation of memes 
 
+genmeme.addEventListener("click", generate);
 
-
-genmeme.addEventListener("click",generate);
 
 
